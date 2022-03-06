7ead1c663f483a005ea3a7f7331ea79b3a5363ae
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-06 14:42:10 +0530

    added control center

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -11,7 +11,7 @@ const initialValues = {
 const AdminPage = () => {
     const { data: session }: any = useSession()
     session && console.log(session)
-    const [addProject, setaddProject]:any = useState()
+    const [addProject, setaddProject]: any = useState()
     const username = session && session.user.username
     const [userData, setuserData] = useState<any>({})
 
@@ -82,9 +82,99 @@ const AdminPage = () => {
                         sign In
                     </button>
                 </div>
-                    : 
+                    :
                     <div>
-                        
+                        <main className='min-h-screen min-w-full border-2 rounded-md grid md:grid-cols-2 grid-cols-1'>
+                            <section className='md:px-8 p-3'>
+                                <h1 className='text-5xl font-extrabold border-b shadow-sm'>
+                                    Control Center
+                                </h1>
+                                <h1 className='text-2xl font-medium text-gray-500 '>
+                                    My link: /{username}
+                                </h1>
+                                <section className=''>
+                                    <h1 className='text-3xl font-bold border-2 rounded-md'>
+                                        Add Projects.
+                                    </h1>
+                                    <div className='p-1 border-2 rounded-md flex'>
+                                        <div className='w-[90%]'>
+                                            <form onSubmit={(e) => addProjectsDB(username, e)}>
+                                                <div className='flex flex-col'>
+                                                    <div className="flex gap-2">
+                                                        <div className="">
+                                                            <label className='text-sm font-semibold'>
+                                                                Project Name
+                                                            </label>
+                                                            <input
+                                                                autoComplete='off'
+                                                                className='w-full p-1 border-2 rounded-md'
+                                                                type='text'
+                                                                name='projectname'
+                                                                value={values.projectname}
+                                                                onChange={handleInputChange}
+                                                            />
+                                                        </div>
+
+                                                        <div>
+                                                            <label className='text-sm font-semibold'>
+                                                                Position
+                                                            </label>
+                                                            <input
+                                                                autoComplete='off'
+                                                                className='w-full p-1 border-2 rounded-md'
+                                                                type='text'
+                                                                name='userposition'
+                                                                value={values.userposition}
+                                                                onChange={handleInputChange}
+                                                            />
+                                                        </div>
+                                                    </div>
+                                                    <label className='text-sm font-semibold'>
+                                                        Link
+                                                    </label>
+                                                    <input
+                                                        autoComplete='off'
+                                                        className='w-full p-1 border-2 rounded-md'
+                                                        type='text'
+                                                        name='link'
+                                                        value={values.link}
+                                                        onChange={handleInputChange}
+                                                    />
+                                                    <label className='text-sm font-semibold'>
+                                                        Note
+                                                    </label>
+                                                    <input
+                                                        autoComplete='off'
+                                                        className='w-full p-1 border-2 rounded-md'
+                                                        type='text'
+                                                        name='note'
+                                                        value={values.note}
+                                                        onChange={handleInputChange}
+                                                    />
+                                                    <button
+                                                        className='w-full p-1 border-2 rounded-md'
+                                                        type='submit'
+                                                    >
+                                                        Add Project
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div className='w-[10%]'>
+                                            delete
+                                            toggle show
+                                        </div>
+                                    </div>
+
+                                </section>
+
+                            </section>
+                            {
+                                session && <section>
+                                    <iframe src={`http://localhost:3000/${username}`} allowFullScreen width='400' height='500' frameBorder="0"></iframe>
+                                </section>
+                            }
+                        </main>
                     </div>
             }
         </div>
